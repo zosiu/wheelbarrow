c11b6317651abac03dbd282bcc033508fd14254d
zs <baloghzsof@gmail.com>
2014-10-13 01:57:43 +0200

    Added Bucketeer class.

---
+++ lib/wheelbarrow/bucketeer.rb
@@ -0,0 +1,59 @@
+require 'singleton'
+require 'json'
+require_relative 'utils'
+require_relative 'authenticator'
+
+module Wheelbarrow
+  class Bucketeer
+    include Singleton
+
+    def initialize
+      @bb_client = Authenticator.instance.oauth_client
+    end
+
+    def send_pull_request!(target_branch: 'master',
+                           title: Utils.last_commit_message,
+                           description: '')
+      payload = pull_request_body(target_branch, title, description).to_json
+
+      res = @bb_client.post pull_request_url,
+                            payload,
+                            'Content-Type' => 'application/json'
+      JSON.parse res.body
+    end
+
+    private
+
+    def pull_request_body(target_branch, title, description)
+      {
+        title: title,
+        description: description,
+        source: pull_request_source,
+        destination: pull_request_destination(target_branch)
+      }
+    end
+
+    def pull_request_source
+      {
+        branch: {
+          name: Utils.current_branch
+        },
+        repository: {
+          full_name: Utils.repo_name
+        }
+      }
+    end
+
+    def pull_request_destination(target_branch)
+      {
+        branch: {
+          name: target_branch
+        }
+      }
+    end
+
+    def pull_request_url
+      "/api/2.0/repositories/#{Utils.repo_name}/pullrequests/"
+    end
+  end
+end
